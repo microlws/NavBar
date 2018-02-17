c2cfcb90a62b45737dc12df6db29fa91e4f7f3ef
microlws <microlws@gmail.com>
2018-02-17 14:13:07 +0100

    til igor (i need help)

---
+++ src/components/NavButton/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './index.scss';
+
+
+const NavButton = () => (
+  <button className='nav__button'>PURCHASE</button>
+);
+
+export default NavButton;
--- src/containers/Nav/index.jsx
+++ src/containers/Nav/index.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import NavList from 'components/NavList';
+import NavButton from 'components/NavButton';
 import './index.scss';
 
 
 const Nav = () => (
   <div className='nav__container'>
-    <div className='nav__title-container'>
+    <div className='nav__container-title'>
       <h1 className='nav__title-text'>Box.</h1>
     </div>
-    <div className='nav__list-container'>
+    <div className='nav__container-list'>
       <NavList />
     </div>
-    <div className='nav__button-container' />
+    <div className='nav__container-button'>
+      <NavButton />
+    </div>
   </div>
 );
 
